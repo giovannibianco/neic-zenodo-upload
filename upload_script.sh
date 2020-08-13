6373ccd2cb3175298d2115174a9322cd4f91ec21
giovannibianco <john.white@cern.ch>
2020-08-13 12:01:27 +0200

    Helper upload script Changes to be committed: new file:   upload_script.sh

---
+++ upload_script.sh
@@ -0,0 +1,7 @@
+# NeIC E3DDS Project Deliverable 2 "On-site data processing simulation"
+
+./upload_to_zenodo.py -p /home/jwhite/Downloads/\
+		      -i E3DDS_Deliverable_2.pdf\
+                      -d e3dds-do2-desc.txt\
+		      -m e3dds-authors.txt\
+		      -A "EXFScetJMy9oK8WXpx0nABpDvQrCLnd6jfgyptM3k5BLrdt0hqhZfzsKE1gh"
